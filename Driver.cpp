b5a90e38bc202787042a95f9e3124d33a58acb6f
Natalie Patten <73899823+nataliepatten@users.noreply.github.com>
2022-04-20 21:46:44 -0400

    cleaned up code, added comments

--- Driver.cpp
+++ Driver.cpp
@@ -14,33 +14,36 @@ using namespace std;
 // used https://www.cplusplus.com/reference/ for reference
 // used https://www.geeksforgeeks.org/measure-execution-time-function-cpp/ for reference on displaying runtime
 int main() {
+    // import flight data
     string fileName = "updated_2008.csv";
-   /* cin >> fileName;*/
 
     fstream inputFile;
     inputFile.open(fileName, ios::in);
     string line;
 
     short int month, delay;
-    bool isMonth = true;
     int count = 0;
     int index = 0;
-    int rows = 400000;
-    pair<short int, short int>* flights = new pair<short int, short int>[rows];
+    int rows = 463128; // number of data points
+    pair<short int, short int>* flights = new pair<short int, short int>[rows];  // pair<delay, month>
 
     while (getline(inputFile, line)) {
         if (count == rows)
             break;
         string temp;
         temp = line.substr(0, line.find(','));
+
+        // get first number, the month
         if (temp != "NA") {
             month = stoi(temp);
         }
 
+        // get second number, the delay time
         temp = line.substr(line.find(',') + 1, line.size() - line.find(','));
         if (temp != "NA") {
             delay = stoi(temp);
             pair<short int, short int> newPair = make_pair(delay, month);
+            // add pair to flights array
             flights[index] = newPair;
             index++;
         }
@@ -49,32 +52,37 @@ int main() {
 
     cout << "Finished reading in flight data..." << endl;
 
-    pair<short int, short int>* temp = new pair<short int, short int>[count];
-    for (int i = 0; i < count; i++)
+    // make a copy of flights to run 2 sorting algorithms
+    pair<short int, short int>* temp = new pair<short int, short int>[index];
+    for (int i = 0; i < index; i++)
         temp[i] = flights[i];
 
+    // run each algorithm and record the runtime for both
     cout << "Performing merge sort..." << endl;
     auto start = high_resolution_clock::now();
-    mergeSort(flights, 0, rows);
+    mergeSort(flights, 0, index);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Using merge sort took " << duration.count() << " microseconds!" << endl;
 
     cout << "Performing quick sort..." << endl;
     start = high_resolution_clock::now();
-    quickSort(temp, 0, rows);
+    quickSort(temp, 0, index);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     cout << "Using quick sort took " << duration.count() << " microseconds!" << endl;
 
+    // initialise number of delays per month to 0
     int months[13];
     for (int i = 1; i < 13; i++)
         months[i] = 0;
 
-    for (int i = 0; i < 0.25 * rows; i++) {
+    // take top 25% delay times (best delay times/no delay/early arrival) and record the month for each
+    for (int i = 0; i < 0.25 * index; i++) {
         months[flights[i].second]++;
     }
 
+    // find the month with the most instances
     int best = 0;
     int bestMonth = 0;
     for (int i = 1; i < 13; i++) {
@@ -85,13 +93,16 @@ int main() {
     }
     cout << "The month with the most 'best' delays (including early arrivals!) is month number " << bestMonth << "!" << endl;
 
+    // initialise number of delays per month to 0
     for (int i = 1; i < 13; i++)
         months[i] = 0;
 
-    for (int i = rows - 1; i > 0.75 * rows; i--) {
+    // take bottom 25% delay times (worst delay times) and record the month for each
+    for (int i = index - 1; i > 0.75 * index; i--) {
         months[flights[i].second]++;
     }
 
+    // find the month with the most instances
     int worst = 0;
     int worstMonth = 0;
     for (int i = 1; i < 13; i++) {
